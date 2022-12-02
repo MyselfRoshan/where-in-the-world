5d8e03aad22190c12115deec95e09c513c980db9
MyselfRoshan <roshanghimire699@gmail.com>
2022-12-03 00:03:51 +0545

    feat:added featch api timeout

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,21 @@ function App() {
   useEffect(() => {
     const baseUrl = 'https://restcountries.com/';
     const version = 'v3.1';
-    async function getCountriesInfo() {
+    const getData = async function getCountriesInfo() {
       const response = await fetch(`${baseUrl}${version}/${searchType}`);
       const data = await response.json();
       SetCountriesInfo(data);
-    }
-    getCountriesInfo();
+    };
+    // Function launches after 0.5 seconds (1500 ms) of the last keystroke
+    // On first render you don't want to launch anything
+    // Thus, you check if the user typed a query at first
+    let apiCallTimer = setTimeout(() => {
+      if (searchType) getData();
+    }, 500);
+    // If useEffect() relaunches, you clear the function
+    // That means, the previous function won't launch
+    // Thus, won't send a request to the API
+    return () => clearTimeout(apiCallTimer);
   }, [searchType]);
 
   const Countries = countriesInfo.map((countryInfo) => {
